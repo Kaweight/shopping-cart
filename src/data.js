db2acc8f0482e41cf3189c6f2da9df1252b91540
Kamil Mazelanik <kamilmaz9206@gmail.com>
2021-02-22 09:25:11 +0100

    add files

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Macbook',
+      price: 1400,
+      image: '../images/macbook.jpg',
+    },
+    {
+      id: '2',
+      name: 'Old Car',
+      price: 2400,
+      image: '../images/old-car.jpg',
+    },
+    {
+      id: '3',
+      name: 'Watch',
+      price: 1000,
+      image: '../images/watch.jpg',
+    },
+  ],
+};
+
+export default data;
